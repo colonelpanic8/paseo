898d4fe99b1d12d0c33b2ba43584df0aa267101c
Ivan Malison <IvanMalison@gmail.com>
2026-09-18 19:02:13 -0700

    fork-assembler: fixup fix/workspace-message-timestamps

--- packages/server/src/server/session.ts
+++ packages/server/src/server/session.ts
@@ -54,7 +54,6 @@ import {
   toAgentPersistenceHandle,
 } from "./persistence-hooks.js";
 import { ensureAgentLoaded, ensureUnarchivedAgentLoaded } from "./agent/agent-loading.js";
-import { collectAgentMessageActivity } from "./agent/message-activity.js";
 import {
   sendPromptToAgent,
   waitForAgentRunStartWithTimeout,
